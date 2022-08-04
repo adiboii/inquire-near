db81b626cfc1b426fbc0352c56b53ffa77e2f1d6
adi.boyii <adrianpauln.reyes@gmail.com>
2022-08-04 10:53:06 +0800

    added app theme constants

---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const Color textColor = Color(0xff201F32);
+const Color buttonText = Color(0xffffffff);
+const Color primary = Color(0xff007AFF);
+const Color gray = Color(0xFFECECEC);
+const Color secondary =  Color(0xFF3B4B84);
+const Color red = Color(0xFFFF5151);
+
+//Text Themes
+
+const TextStyle header1 = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 32,
+  fontWeight: FontWeight.w800,
+  color: textColor,
+);
+
+const TextStyle header2 = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  color: textColor,
+);
+
+const TextStyle header3 = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  color: textColor,
+);
+
+const TextStyle header4 = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  color: textColor,
+);
