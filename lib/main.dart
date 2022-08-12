2be1e24a0cee1296e98d5fab1c28a16ead7618ab
adi.boyii <adrianpauln.reyes@gmail.com>
2022-08-12 12:56:09 +0800

    added new screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 //import 'package:inquire_near/screens/routes.dart';
 import 'package:inquire_near/screens/authenticate/login_screen.dart';
 import 'package:inquire_near/screens/authenticate/sign_up_screen.dart';
+import 'package:inquire_near/screens/client/client_dashboard_screen.dart';
+import 'package:inquire_near/screens/client/inquiry_list_screen.dart';
 import 'package:inquire_near/screens/common/landing_screen.dart';
 import 'package:inquire_near/screens/common/onboarding_screen.dart';
+import 'package:inquire_near/screens/common/profile_screen.dart';
 import 'package:inquire_near/screens/common/splash_screen.dart';
-import 'package:inquire_near/screens/top_up.dart';
-import 'package:inquire_near/screens/user_wallet.dart';
+import 'package:inquire_near/screens/common/top_up.dart';
+import 'package:inquire_near/screens/common/user_wallet.dart';
 
 void main() {
   runApp(const InquireNear());
@@ -20,7 +23,7 @@ class InquireNear extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Inquire Near',
-      initialRoute: '/splash',
+      initialRoute: '/client_dashboard',
       routes: {
         '/splash': (context) => SplashScreen(),
         '/landing': (context) => LandingScreen(),
@@ -29,6 +32,9 @@ class InquireNear extends StatelessWidget {
         '/onboarding': (context) => OnboardingScreen(),
         '/user_wallet': (context) => UserWalletScreen(),
         '/top_up': (context) => TopUpScreen(),
+        '/client_dashboard': (context) => ClientDashboardScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/inquiry_list': (context) => InquiryListScreen(),
       },
     );
   }
