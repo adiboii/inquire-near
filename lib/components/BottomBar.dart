d14e46541927eed3e9f8395afeec6762eb4a4c82
adi.boyii <adrianpauln.reyes@gmail.com>
2022-08-16 15:10:31 +0800

    added bottombar component for adding images

---
+++ lib/components/BottomBar.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class BottomBar extends StatefulWidget {
+  final Function(File) onIconSelected;
+  BottomBar({Key? key, required this.onIconSelected}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  void setImage(ImageSource source) {
+    ImagePicker()
+        .pickImage(source: source, imageQuality: 20)
+        .then((XFile? file) {
+      setState(() {
+        // _image = file;
+        widget.onIconSelected(File(file!.path));
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      decoration: BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.grey)),
+          color: Colors.white),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            onPressed: () {
+              setImage(ImageSource.gallery);
+            },
+            icon: Icon(Icons.image),
+          ),
+          IconButton(
+            onPressed: () {
+              setImage(ImageSource.camera);
+            },
+            icon: Icon(Icons.camera_alt_sharp),
+          ),
+        ],
+      ),
+    );
+  }
+}
