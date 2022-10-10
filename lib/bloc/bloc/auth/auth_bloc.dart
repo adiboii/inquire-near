e8bca19642354a570f62b8504cfdb43f7f9b2024
ap-reyes <adrianpauln.reyes@gmail.com>
2022-10-10 10:36:12 +0800

    refactors on<Event> to use defined methods

--- lib/bloc/bloc/auth/auth_bloc.dart
+++ lib/bloc/bloc/auth/auth_bloc.dart
@@ -1,6 +1,3 @@
-// Dart imports:
-import 'dart:developer';
-
 // Flutter imports:
 import 'package:flutter/cupertino.dart';
 
@@ -18,49 +15,54 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final AuthRepository authRepository;
 
   AuthBloc({required this.authRepository}) : super(Unauthenticated()) {
-    on<SignInRequested>((event, emit) async {
-      emit(Loading());
-      try {
-        await authRepository.signIn(
-            email: event.email, password: event.password);
+    on<SignInRequested>(_onSignInRequested);
+    on<SignUpRequested>(_onSignUpRequested);
+    on<GoogleSignInRequested>(_onGoogleSignInRequested);
+    on<SignOutRequested>(_onSignOutRequested);
+  }
 
-        emit(Authenticated());
-      } on LogInWithEmailAndPasswordFailure catch (e) {
-        emit(AuthError(e.message));
-        emit(Unauthenticated());
-      }
-    });
+  _onSignInRequested(SignInRequested event, Emitter<AuthState> emit) async {
+    emit(Loading());
+    try {
+      await authRepository.signIn(email: event.email, password: event.password);
 
-    on<SignUpRequested>((event, emit) async {
-      emit(Loading());
-      try {
-        await authRepository.signUp(
-            firstName: event.firstName,
-            lastName: event.lastName,
-            email: event.email,
-            password: event.password);
-        emit(Authenticated());
-      } on SignUpWithEmailAndPasswordFailure catch (e) {
-        emit(AuthError(e.message));
-        emit(Unauthenticated());
-      }
-    });
-
-    on<GoogleSignInRequested>((event, emit) async {
-      emit(Loading());
-      try {
-        await authRepository.signInWithGoogle();
-        emit(Authenticated());
-      } on LogInWithGoogleFailure catch (e) {
-        emit(AuthError(e.message));
-        emit(Unauthenticated());
-      }
-    });
-
-    on<SignOutRequested>((event, emit) async {
-      emit(Loading());
-      await authRepository.signOut();
+      emit(Authenticated());
+    } on LogInWithEmailAndPasswordFailure catch (e) {
+      emit(AuthError(e.message));
       emit(Unauthenticated());
-    });
+    }
+  }
+
+  _onSignUpRequested(SignUpRequested event, Emitter<AuthState> emit) async {
+    emit(Loading());
+    try {
+      await authRepository.signUp(
+          firstName: event.firstName,
+          lastName: event.lastName,
+          email: event.email,
+          password: event.password);
+      emit(Authenticated());
+    } on SignUpWithEmailAndPasswordFailure catch (e) {
+      emit(AuthError(e.message));
+      emit(Unauthenticated());
+    }
+  }
+
+  _onGoogleSignInRequested(
+      GoogleSignInRequested event, Emitter<AuthState> emit) async {
+    emit(Loading());
+    try {
+      await authRepository.signInWithGoogle();
+      emit(Authenticated());
+    } on LogInWithGoogleFailure catch (e) {
+      emit(AuthError(e.message));
+      emit(Unauthenticated());
+    }
+  }
+
+  _onSignOutRequested(SignOutRequested event, Emitter<AuthState> emit) async {
+    emit(Loading());
+    await authRepository.signOut();
+    emit(Unauthenticated());
   }
 }
