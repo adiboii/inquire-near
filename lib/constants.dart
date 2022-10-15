6f638979d37841e2e18b8f4518c3eb2a9deffb79
Mel Jefferson Gabutan <meljeffersongabutan@Mels-MacBook-Air.local>
2022-10-15 20:26:48 +0800

    creates StoreData class and stores all stores in a list of StoreData objects

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,12 +2,12 @@
 
 // Project imports:
 import 'package:inquire_near/data/models/enums.dart';
+import 'package:inquire_near/data/models/store_data.dart';
 
 const String splash = "/splash";
 
 // Map for Store List enum
 //TODO: to implement
-Map<Store, Map<String, dynamic>> storeList = {};
 
 // Auth Error Codes
 const String invalidEmail = 'invalid-email';
@@ -16,3 +16,30 @@ const String operationNotAllowed = 'operation-not-allowed';
 const String userNotFound = 'user-not-found';
 const String wrongPassword = 'wrong-password';
 const String emailAlreadyInUse = 'email-already-in-use';
+
+final stores = [
+  // Dining
+  StoreData(name: 'Casa Verde', category: 'Dining'),
+  StoreData(name: 'Seafood Island', category: 'Dining'),
+  StoreData(name: 'Bigby\'s', category: 'Dining'),
+  // Entertainment
+  StoreData(name: 'SM Bowling Center', category: 'Entertainment'),
+  StoreData(name: 'Ice Skating Rink', category: 'Entertainment'),
+  StoreData(name: 'Color Museum', category: 'Entertainment'),
+  // Cinema
+  StoreData(name: 'Center Stage', category: 'Cinema'),
+  StoreData(name: 'Director\'s Club', category: 'Cinema'),
+  StoreData(name: 'SM Cinema', category: 'Cinema'),
+  // Gadgets
+  StoreData(name: 'DataBlitz', category: 'Gadgets'),
+  StoreData(name: 'Octagon', category: 'Gadgets'),
+  StoreData(name: 'iStore', category: 'Gadgets'),
+  // Services
+  StoreData(name: 'BDO', category: 'Services'),
+  StoreData(name: 'Bruno\'s Barbers', category: 'Services'),
+  StoreData(name: 'PLDT', category: 'Services'),
+  // Shopping
+  StoreData(name: 'SM Appliance Center', category: 'Shopping'),
+  StoreData(name: 'Watsons', category: 'Shopping'),
+  StoreData(name: 'H&M', category: 'Shopping'),
+];
