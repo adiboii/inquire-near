a2b4280ad89a60dee064fe522b85047102d3e2e3
Cymmer John Maranga <cymmerjohnmaranga@Cymmers-MacBook-Pro.local>
2022-10-20 12:34:22 +0800

    Initial implementation

--- lib/components/status_switch.dart
+++ lib/components/status_switch.dart
@@ -1,70 +1,60 @@
-// Dart imports:
-import 'dart:developer';
-
 // Flutter imports:
 import 'package:flutter/material.dart';
 
 // Package imports:
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 import 'package:lottie/lottie.dart';
 
 // Project imports:
+import 'package:inquire_near/bloc/bloc/inquirer/inquirer_bloc.dart';
 import 'package:inquire_near/themes/app_theme.dart' as theme;
 
 class StatusSwitch extends StatefulWidget {
+  final bool currentStatus;
   const StatusSwitch({
     Key? key,
-    required this.isOnline,
-    required this.screenHeight,
+    required this.currentStatus,
   }) : super(key: key);
-
-  final bool isOnline;
-  final double screenHeight;
 
   @override
   State<StatusSwitch> createState() => _StatusSwitchState();
 }
 
 class _StatusSwitchState extends State<StatusSwitch> {
-  late bool isOnline;
-
   @override
   void initState() {
     super.initState();
-    isOnline = widget.isOnline;
   }
 
   @override
   Widget build(BuildContext context) {
+    InquirerBloc inquirerBloc = BlocProvider.of<InquirerBloc>(context);
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const Text("Status", style: theme.headline),
         FlutterSwitch(
-          activeText: "Online",
-          inactiveText: "Offline",
-          activeColor: theme.primary,
-          inactiveColor: theme.primaryGray,
-          activeTextColor: Colors.white,
-          inactiveTextColor: Colors.white,
-          activeTextFontWeight: FontWeight.w700,
-          inactiveTextFontWeight: FontWeight.w700,
-          value: isOnline,
-          valueFontSize: 15.0,
-          width: 85,
-          height: 30,
-          borderRadius: 20.0,
-          showOnOff: true,
-          onToggle: (val) {
-            setState(() {
-              isOnline = val;
-              log(isOnline.toString());
-            });
-          },
-        ),
-        SizedBox(height: widget.screenHeight * 0.02),
+            activeText: "Online",
+            inactiveText: "Offline",
+            activeColor: theme.primary,
+            inactiveColor: theme.primaryGray,
+            activeTextColor: Colors.white,
+            inactiveTextColor: Colors.white,
+            activeTextFontWeight: FontWeight.w700,
+            inactiveTextFontWeight: FontWeight.w700,
+            value: inquirerBloc.isOnline,
+            valueFontSize: 15.0,
+            width: 85,
+            height: 30,
+            borderRadius: 20.0,
+            showOnOff: true,
+            onToggle: (_) => inquirerBloc.add(ToggleIsOnline())),
+        SizedBox(height: screenHeight * 0.02),
         Lottie.asset("assets/images/lottie/job_search.json",
-            height: widget.screenHeight * 0.30),
+            height: screenHeight * 0.30),
         const Text(
           "Do not put your phone in silent mode\nas you wait for a client.",
           style: theme.footnote,
