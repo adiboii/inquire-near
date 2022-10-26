84b23801547b703702c824639583ce8d22bb7eb3
Mel Jefferson Gabutan <meljeffersongabutan@Mels-MacBook-Air.local>
2022-10-26 18:01:22 +0800

    fixes sign up validator

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:inquire_near/bloc/bloc/auth/auth_bloc.dart';
 import 'package:inquire_near/screens/client/client_dashboard/client_dashboard_screen.dart';
+import 'package:inquire_near/screens/common/onboarding_screen.dart';
+import 'package:inquire_near/screens/common/splash_screen.dart';
 
 class Wrapper extends StatefulWidget {
   const Wrapper({super.key});
@@ -13,6 +16,10 @@ class Wrapper extends StatefulWidget {
 class _WrapperState extends State<Wrapper> {
   @override
   Widget build(BuildContext context) {
+    if (FirebaseAuth.instance.currentUser == null) {
+      return const OnboardingScreen();
+    }
+
     return BlocConsumer<AuthBloc, AuthState>(listener: (context, state) {
       if (state is Unauthenticated) {
         Navigator.of(context)
