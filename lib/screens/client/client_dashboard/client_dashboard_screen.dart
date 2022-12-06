3cb9e3b5361661d8ccf9379f638e6b3924ee79a7
Cymmer John Maranga <cymmerjohnmaranga@Cymmers-MacBook-Pro.local>
2022-12-06 18:52:59 +0800

    Moves app to background on back in ClientDashboard

--- lib/screens/client/client_dashboard/client_dashboard_screen.dart
+++ lib/screens/client/client_dashboard/client_dashboard_screen.dart
@@ -1,4 +1,5 @@
 // Flutter imports:
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
@@ -17,6 +18,7 @@ import 'package:inquire_near/screens/client/client_dashboard/widgets/recent_plac
 import 'package:inquire_near/screens/client/client_dashboard/widgets/search_bar.dart';
 import 'package:inquire_near/screens/common/recent_transactions/recent_transactions_widget.dart';
 import 'package:inquire_near/themes/app_theme.dart' as theme;
+import 'package:move_to_background/move_to_background.dart';
 
 class ClientDashboardScreen extends StatefulWidget {
   const ClientDashboardScreen({Key? key}) : super(key: key);
@@ -47,130 +49,136 @@ class _ClientDashboardScreenState extends State<ClientDashboardScreen> {
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Padding(
-          padding: theme.kScreenPadding.copyWith(top: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, profileRoute);
-                        },
-                        child: CircleAvatar(
-                          radius: screenWidth * 0.05,
-                          backgroundImage: const AssetImage(
-                            'assets/images/illustrations/profile.png',
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: screenWidth * 0.03),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Greeting(
-                            name: BlocProvider.of<AuthBloc>(context)
-                                .user!
-                                .firstName!,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  SwitchUserType(
-                      screenHeight: screenHeight, currentRole: Role.client),
-                ],
-              ),
-              SizedBox(height: screenHeight * 0.02),
-              SearchBar(
-                readOnly: true,
-                onTap: () {
-                  Navigator.pushNamed(context, searchRoute);
-                },
-              ),
-              SizedBox(height: screenHeight * 0.02),
-              const Text(
-                "Categories",
-                style: theme.headline,
-              ),
-              SizedBox(height: screenHeight * 0.02),
-              //TODO: convert to widget (MEL)
-              SizedBox(
-                height: screenHeight * 0.04,
-                child: ListView.builder(
-                  itemCount: categories.length,
-                  itemBuilder: ((context, index) {
-                    return Row(
+      body: WillPopScope(
+        onWillPop: () async {
+          MoveToBackground.moveTaskToBack();
+          return false;
+        },
+        child: SafeArea(
+          child: Padding(
+            padding: theme.kScreenPadding.copyWith(top: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
                       children: [
                         GestureDetector(
                           onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => CategoryScreen(
-                                  category: categories[index],
-                                ),
-                              ),
-                            );
+                            Navigator.pushNamed(context, profileRoute);
                           },
-                          child: Text(
-                            categories[index],
-                            style: theme.callout,
+                          child: CircleAvatar(
+                            radius: screenWidth * 0.05,
+                            backgroundImage: const AssetImage(
+                              'assets/images/illustrations/profile.png',
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: screenWidth * 0.03),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Greeting(
+                              name: BlocProvider.of<AuthBloc>(context)
+                                  .user!
+                                  .firstName!,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SwitchUserType(
+                        screenHeight: screenHeight, currentRole: Role.client),
+                  ],
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                SearchBar(
+                  readOnly: true,
+                  onTap: () {
+                    Navigator.pushNamed(context, searchRoute);
+                  },
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                const Text(
+                  "Categories",
+                  style: theme.headline,
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                //TODO: convert to widget (MEL)
+                SizedBox(
+                  height: screenHeight * 0.04,
+                  child: ListView.builder(
+                    itemCount: categories.length,
+                    itemBuilder: ((context, index) {
+                      return Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CategoryScreen(
+                                    category: categories[index],
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              categories[index],
+                              style: theme.callout,
+                            ),
+                          ),
+                          SizedBox(
+                            width: screenWidth * 0.04,
+                          ),
+                        ],
+                      );
+                    }),
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: screenHeight * 0.025),
+                    const Text(
+                      "Featured Places",
+                      style: theme.headline,
+                    ),
+                    SizedBox(height: screenHeight * 0.025),
+                    //TODO: convert to widget (MEL)
+                    Row(
+                      children: [
+                        const Expanded(
+                          child: FeaturedPlace(
+                            storeName: 'BDO',
+                            imageFilePath: 'assets/images/logos/bdo.png',
                           ),
                         ),
                         SizedBox(
-                          width: screenWidth * 0.04,
+                          width: screenWidth * 0.05,
+                        ),
+                        const Expanded(
+                          child: FeaturedPlace(
+                            storeName: 'H&M',
+                            imageFilePath: 'assets/images/logos/h&m.png',
+                          ),
                         ),
                       ],
-                    );
-                  }),
-                  scrollDirection: Axis.horizontal,
+                    )
+                  ],
                 ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: screenHeight * 0.025),
-                  const Text(
-                    "Featured Places",
-                    style: theme.headline,
-                  ),
-                  SizedBox(height: screenHeight * 0.025),
-                  //TODO: convert to widget (MEL)
-                  Row(
-                    children: [
-                      const Expanded(
-                        child: FeaturedPlace(
-                          storeName: 'BDO',
-                          imageFilePath: 'assets/images/logos/bdo.png',
-                        ),
-                      ),
-                      SizedBox(
-                        width: screenWidth * 0.05,
-                      ),
-                      const Expanded(
-                        child: FeaturedPlace(
-                          storeName: 'H&M',
-                          imageFilePath: 'assets/images/logos/h&m.png',
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(
-                height: screenHeight * 0.02,
-              ),
-              const RecentTransactions(role: Role.client),
-            ],
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                const RecentTransactions(role: Role.client),
+              ],
+            ),
           ),
         ),
       ),
