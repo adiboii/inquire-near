688a22d5a83b11aa1dafbff1bce60cd8d7bb1475
Cymmer John Maranga <cymmerjohnmaranga@Cymmers-MacBook-Pro.local>
2022-12-09 17:12:40 +0800

    Fixes review on submit for view recent transactions

--- lib/screens/common/review_screen.dart
+++ lib/screens/common/review_screen.dart
@@ -73,119 +73,125 @@ class _FeedbackScreenState extends State<FeedbackScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Padding(
-            padding: theme.kScreenPadding,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      const AutoSizeText(
-                        'Tell us about your experience',
-                        style: theme.headline,
-                      ),
-                      SizedBox(
-                        height: screenHeight * 0.02,
-                      ),
-                      const AutoSizeText(
-                        'Your feedback will help us\nimprove your inquiring needs',
-                        textAlign: TextAlign.center,
-                        style: theme.footnote,
-                      ),
-                      SizedBox(
-                        height: screenHeight * 0.04,
-                      ),
-                      const BorderedProfilePicture(),
-                      SizedBox(
-                        height: screenHeight * 0.02,
-                      ),
-                      AutoSizeText(
-                        "${user!.firstName} ${user!.lastName}",
-                        style: theme.subheadBold,
-                      ),
-                      SizedBox(
-                        height: screenHeight * 0.02,
-                      ),
-                      RatingBar(
-                        ratingWidget: RatingWidget(
-                          full: const Icon(
-                            Icons.star,
-                            color: theme.primaryYellow,
-                          ),
-                          half: const Icon(
-                            Icons.star,
-                            color: Colors.grey,
-                          ),
-                          empty: const Icon(
-                            Icons.star,
-                            color: Colors.grey,
-                          ),
+          child: BlocListener<FeedbackBloc, FeedbackState>(
+            listener: (context, state) {
+              if (state is FeedbackSuccess) {
+                Navigator.pushNamedAndRemoveUntil(
+                    context, wrapperRoute, (route) => false);
+              }
+            },
+            child: Padding(
+              padding: theme.kScreenPadding,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        const AutoSizeText(
+                          'Tell us about your experience',
+                          style: theme.headline,
                         ),
-                        onRatingUpdate: (rating) {
-                          setState(() {
-                            final newRating =
-                                int.parse(rating.toString().split('.')[0]);
-                            this.rating = newRating;
-                            rating == 0 ? hasRated = false : hasRated = true;
-                          });
-                        },
-                        itemSize: screenHeight * 0.035,
-                      ),
-                      SizedBox(
-                        height: screenHeight * 0.02,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(
-                          left: 15.0,
-                          right: 15.0,
+                        SizedBox(
+                          height: screenHeight * 0.02,
                         ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                            4.0,
-                          ),
-                          border: Border.all(
-                            color: Colors.grey,
-                          ),
+                        const AutoSizeText(
+                          'Your feedback will help us\nimprove your inquiring needs',
+                          textAlign: TextAlign.center,
+                          style: theme.footnote,
                         ),
-                        width: double.infinity,
-                        height: screenHeight * 0.38,
-                        child: TextField(
-                          controller: _reviewTextController,
-                          decoration: const InputDecoration(
-                            hintText: 'Leave a Review',
-                            hintStyle: theme.caption1,
-                            border: InputBorder.none,
-                          ),
-                          keyboardType: TextInputType.multiline,
-                          maxLines: null,
+                        SizedBox(
+                          height: screenHeight * 0.04,
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: screenHeight * 0.015),
-                  ButtonOutline(
-                    color: (hasRated) ? theme.primary : theme.gray,
-                    textColor: (hasRated) ? theme.primary : theme.gray,
-                    label: 'Finish',
-                    height: screenHeight * 0.07,
-                    style: theme.caption1Bold,
-                    onTap: (hasRated)
-                        ? () {
-                            _submitFeedback(context, role);
-                            Navigator.pushNamedAndRemoveUntil(context,
-                                clientDashboardRoute, (route) => false);
-                          }
-                        : () {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text(
-                                    "You are required to rate the client."),
-                              ),
-                            );
+                        const BorderedProfilePicture(),
+                        SizedBox(
+                          height: screenHeight * 0.02,
+                        ),
+                        AutoSizeText(
+                          "${user!.firstName} ${user!.lastName}",
+                          style: theme.subheadBold,
+                        ),
+                        SizedBox(
+                          height: screenHeight * 0.02,
+                        ),
+                        RatingBar(
+                          ratingWidget: RatingWidget(
+                            full: const Icon(
+                              Icons.star,
+                              color: theme.primaryYellow,
+                            ),
+                            half: const Icon(
+                              Icons.star,
+                              color: Colors.grey,
+                            ),
+                            empty: const Icon(
+                              Icons.star,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          onRatingUpdate: (rating) {
+                            setState(() {
+                              final newRating =
+                                  int.parse(rating.toString().split('.')[0]);
+                              this.rating = newRating;
+                              rating == 0 ? hasRated = false : hasRated = true;
+                            });
                           },
-                  ),
-                ],
+                          itemSize: screenHeight * 0.035,
+                        ),
+                        SizedBox(
+                          height: screenHeight * 0.02,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                            left: 15.0,
+                            right: 15.0,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              4.0,
+                            ),
+                            border: Border.all(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          width: double.infinity,
+                          height: screenHeight * 0.38,
+                          child: TextField(
+                            controller: _reviewTextController,
+                            decoration: const InputDecoration(
+                              hintText: 'Leave a Review',
+                              hintStyle: theme.caption1,
+                              border: InputBorder.none,
+                            ),
+                            keyboardType: TextInputType.multiline,
+                            maxLines: null,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenHeight * 0.015),
+                    ButtonOutline(
+                      color: (hasRated) ? theme.primary : theme.gray,
+                      textColor: (hasRated) ? theme.primary : theme.gray,
+                      label: 'Finish',
+                      height: screenHeight * 0.07,
+                      style: theme.caption1Bold,
+                      onTap: (hasRated)
+                          ? () {
+                              _submitFeedback(context, role);
+                            }
+                          : () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text(
+                                      "You are required to rate the client."),
+                                ),
+                              );
+                            },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
